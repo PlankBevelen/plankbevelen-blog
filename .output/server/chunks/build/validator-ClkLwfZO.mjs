26a25c0c3ac8e2dec3e5555851a271a9ec0dfdb9
PlankBevelen <3585138040@qq.com>
2026-07-19 02:13:30 +0800

    Remove unused virtual source files and server index, cleaning up the output directory

--- .output/server/chunks/build/validator-ClkLwfZO.mjs
+++
@@ -1,6 +0,0 @@
-import { t as componentSizes } from './server.mjs';
-
-const isValidComponentSize = (val) => ["", ...componentSizes].includes(val);
-
-export { isValidComponentSize as i };
-//# sourceMappingURL=validator-ClkLwfZO.mjs.map
