5841c5b73c054f59033702b2a9925d527faa548e
PlankBevelen <145191608+PlankBevelen@users.noreply.github.com>
2026-07-23 15:38:14 +0800

    Rename 'keepAlive' parameter to 'remember' in login

--- app/services/admin.service.ts
+++ app/services/admin.service.ts
@@ -2,8 +2,8 @@ import http from "~/utils/http"
 import type { ApiResponse } from "~/types/api"
 
 class AdminService {
-  async login( account: string, password: string, keepAlive?: boolean ) {
-    return await http.post<ApiResponse<{ token: string }>>('/admin/login', { account, password, keepAlive })
+  async login(account: string, password: string, remember?: boolean) {
+    return await http.post<ApiResponse<{ token: string }>>('/admin/login', { account, password, remember })
   }
   async logout() {
     return await http.post<ApiResponse>('/admin/logout')
